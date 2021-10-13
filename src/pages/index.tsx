945964abebc03fed1312cfbcfebe1a881047a5a6
tamagosann <hssvs619@gmail.com>
2021-10-13 23:34:12 +0900

    ci-check

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ const Home: NextPage = () => {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          CI うごくか？ by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
